2120f3590b19115e66776ae6ba036aa4a65f2456
VinylTheDuster <william.salembien@outlook.com>
2025-10-08 18:06:11 +0200

    Added a data folder for the list of articles + added dotenv & supabase packages + gitignore

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Devlog API
+
+## But
+
+* Simuler une interface web côté serveur pour créer, modifier et supprimer des articles et des étiquettes/tags d'articles.
+* Distribuer des données JSON et Markdown.
+* Faire le lien entre une base SQL et 
--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const express = require('express');
+import express from "express";
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+app.use(express.json());
+
+// Supabase intialization
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY
+);
+
+app.get("/", (req, res) => {
+    res.send("API online.");
+});
+
+app.get("/articles", (req, res) => {
+    
+});
+
+app.post("/adminlogin", (req, res) => {
+    const { username, password, fruit } = req.body;
+});
